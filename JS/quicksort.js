23b0cd960675b32ea0c92391a73f9fd185a0e0dc
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-31 13:22:34 -0500

    Function does one round of choosing a pivot and checking if any integers before it are less than; Once the pivot is reached, the pivot it swaped with the first index of the sub-array

--- JS/quicksort.js
+++ JS/quicksort.js
@@ -3,9 +3,21 @@ console.log('Quick Sort!');
 
 const quickSort = (arr) => {
 	let pivot = arr[arr.length -  1];
+	let indx0 = 0;
 
-	console.log(pivot);
+	for (let i = 0, len = arr.length; i < len; i ++) {
+		if (arr[i] < pivot) {
+			const temp = arr[indx0];
+			arr[indx0] = arr[i];
+			arr[i] = temp;
+			indx0 ++; 
+		};
+	};
+	arr[arr.length - 1] = arr[indx0];
+	arr[indx0] = pivot
+	console.log(arr);
 };
 
 
 quickSort([7, 3, 0, 9, 2, 1, 5]);
+
