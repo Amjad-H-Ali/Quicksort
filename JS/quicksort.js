80cf152a7a9fef779d7a7cf077ab6b12277ab5aa
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-01 09:33:06 -0500

    Added comments

--- JS/quicksort.js
+++ JS/quicksort.js
@@ -2,12 +2,15 @@ console.log('Quick Sort!');
 
 
 const quickSort = (arr) => {
+	// Exit code for recurive function
 	if (arr.length <= 1) {
 		return arr;
 	};
-
+	// Integer less than pivot go in left sub array
 	let left = [];
+	// Integer greater than pivot go in right sub array
 	let right = [];
+	// We'll arbitrarly chose the last element as our pivot
 	let pivot = arr[arr.length - 1];
 
 	for (let i = 0, len = arr.length - 1; i < len; i ++) {
@@ -18,9 +21,11 @@ const quickSort = (arr) => {
 			right.push(arr[i]);
 		};
 	};
+	// Repeat the process with the sub-arrays
 	return [...quickSort(left),pivot,...quickSort(right)];
 };
 
 
 console.log(quickSort([7, 3, 0, 9, 2, 1, 5, 7]));
 
+        
